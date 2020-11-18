4c5c37c0942904dc7ca8c33d476b81ea30a3356c
person12-max <71512968+person12-max@users.noreply.github.com>
2020-11-18 12:36:02 -0500

    Update omp4.c

--- lab-09-main/omp4.c
+++ lab-09-main/omp4.c
@@ -1,2 +1,22 @@
 // gcc -std=c99 -fopenmp omp4.c -o omp4
 // You fill in
+#include <stdio.h>
+
+int fib_recursive(int n){
+  // base case
+  if(n < 2){
+   return 1;
+  }
+  // Accumulate our result 
+  return fib_recursive(n-1)+fib_recursive(n-2);
+}
+
+
+int main(){
+  
+  // Computes the 41st number(n+1) in the fibonacci sequence
+  printf("%d ",fib_recursive(40));
+  printf("\n");
+  
+  return 0;
+}
