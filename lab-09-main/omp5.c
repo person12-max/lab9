a50dfcdb138712e871eea0eac8cd764252ed5a3d
person12-max <71512968+person12-max@users.noreply.github.com>
2020-11-18 12:37:26 -0500

    Update omp5.c

--- lab-09-main/omp5.c
+++ lab-09-main/omp5.c
@@ -1,2 +1,22 @@
 // gcc -std=c99 -fopenmp omp5.c -o omp5
 // You fill in
+#include <stdio.h>
+
+static long num_steps = 100000000;
+double step;
+
+int main(){
+  int i;
+  double x, pi, sum = 0.0;
+  
+  step = 1.0/(double)num_steps;
+  for(i =0; i < num_steps; i++){
+    x = (i+0.5)*step;
+    sum = sum+ 4.0/(1.0+x*x);
+  }
+  pi = step * sum;
+  
+  printf("PI = %f\n", pi);
+ 
+  return 0;
+}
